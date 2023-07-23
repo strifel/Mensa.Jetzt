4d6f8e02d5dd6280c8ab2c05dd972a9472022559
Felix <info@strifel.de>
2023-07-23 18:07:08 +0200

    Add login support

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if (isset($_COOKIE['PHPSESSID'])) {
+  // Only start session (would set cookie) if we have consent by
+  // user by logging in
+  session_start();
+}
 if (date("N") == 6) {
   $filename = "../data/".date("Y-m-d", time() + 60*60*24*2).".db";
   $humanReadableDay = "Montag";
@@ -18,8 +23,16 @@ if (date("N") == 6) {
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	if (preg_match("/^[a-zA-Z0-9äöüß]{1,20}$/s", $_POST['name']) != 1) die("Bad name data");
 	if (preg_match("/^1[1-4]:[0-5][0-9] Uhr$/s", $_POST['time']) != 1) die("Bad time data");
-	$fh = fopen($filename, "a") or die("Unable to open database!");	
-	$txt = $_POST['name'].','.$_POST['time'];
+	$fh = fopen($filename, "a") or die("Unable to open database!");
+
+  // Logged out users shall have id -1
+  if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+  } else {
+    $user_id = "-1";
+  }
+
+	$txt = $_POST['name'].','.$_POST['time'].','.$user_id;
 	fwrite($fh, $txt."\n");
 	fclose($fh);
 	if ($_POST['saveName'] == 'on') {
@@ -38,8 +51,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <script src="/bootstrap/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </head>
   <body>
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light justify-content-between" style="padding: 0">
       <span class="navbar-brand mb-0 h1" style="padding-left: 1rem">Mensa.JETZT</span>
+      <form class="form-inline">
+        <?php if (!isset($_SESSION['user_id'])) { ?>
+          <a href="/oauth.php" class="btn btn-outline-success my-2 my-sm-0" style="margin-right: 1rem;margin-top: 10px !important">Login</a>
+        <?php } else { ?>
+          <button onclick='document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });location.reload()' class="btn btn-outline-success my-2 my-sm-0" style="margin-right: 1rem;margin-top: 10px !important">Logout</button>
+        <?php } ?>
+      </form>
     </nav>
   <div id="content" style="padding: 2rem;">
     <h2><?php echo $humanReadableDay; ?> in der Mensa</h2><br>
@@ -68,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
       <div class="card-body">
         <h5 class="card-title">Du bist heute auch in der Mensa?</h5>
       <form method="POST">
-      <input class="form-control" type="text" name="name" pattern="^[a-zA-Z0-9äöüß]{1,20}$" placeholder="Gebe hier deinen Namen ein" <?php if (isset($_COOKIE['save-name'])) { ?> value="<?php echo $_COOKIE['save-name'] ?>" readonly <?php } ?> /><br>
+      <input class="form-control" type="text" name="name" pattern="^[a-zA-Z0-9äöüß]{1,20}$" placeholder="Gebe hier deinen Namen ein" value="<?php if (isset($_COOKIE['save-name'])) echo $_COOKIE['save-name']; else if (isset($_SESSION['name'])) echo $_SESSION['name']; ?>" /><br>
       <select class="form-control" name="time">
         <option value="11:30 Uhr">11:30 Uhr</option>
         <option value="11:45 Uhr">11:45 Uhr</option>
---
+++ oauth.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'config.php';
+if (!isset($_GET['code'])) {
+    header("Location: ".$oauth_request_url, true, 303);
+    exit();
+}
+
+$code = $_GET['code'];
+$parameters = 'client_id='.$oauth_client_id.'&code='.$code.'&grant_type=authorization_code&redirect_uri='.$oauth_redirect_url.'&client_secret='.$oauth_client_secret;
+
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => $parameters,
+    ],
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($oauth_token_url, false, $context);
+if ($result === false) die("auth failed");
+$access_token = json_decode($result)->{'access_token'};
+
+$options = [
+    'http' => [
+        'header' => "Authorization: Bearer ".$access_token."\r\n",
+        'method' => 'GET'
+    ],
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($oauth_userinfo_url, false, $context);
+if ($result === false) die("request failed");
+$data = json_decode($result, true);
+session_start();
+$_SESSION['user_id'] = $data['sub'];
+if (isset($data['name'])) {
+    $_SESSION['name'] = $data['name'];
+}
+header("Location: /");
+exit();
