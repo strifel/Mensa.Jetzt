a2a1b14354f2b18cc04e40e906ea50ee9fa36725
strifel <info@strifel.de>
2024-05-13 21:21:36 +0200

    Allow CORS for API

--- api/entries/index.php
+++ api/entries/index.php
@@ -1,6 +1,8 @@
 <?php
 include_once '../../db.php';
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
 
 $dateConfig = getCurrentDayConfiguration();
 
